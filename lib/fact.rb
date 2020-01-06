12b85d05da11a42f7119e0b0002a647d832fcda4
github username <github email address>
2020-01-06 16:09:21 -0600

    Corrected associations on User model and Fact model.

--- lib/fact.rb
+++ lib/fact.rb
@@ -1,3 +1,4 @@
 class fact < ActiveRecord::Base
+    has_many :users_facts
     has_many :users, through :users_facts
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :users_facts
     has_many :facts, through :users_facts
+
 end
