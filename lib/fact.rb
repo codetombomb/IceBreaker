641d6bac6d2f7391c028bcea41f965915050ec87
github username <github email address>
2020-01-06 16:15:49 -0600

    Added colon on through on fact and user model associations.

--- lib/fact.rb
+++ lib/fact.rb
@@ -1,4 +1,4 @@
-class fact < ActiveRecord::Base
+class Fact < ActiveRecord::Base
     has_many :users_facts
-    has_many :users, through :users_facts
+    has_many :users, through: :users_facts
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
     has_many :users_facts
-    has_many :facts, through :users_facts
-
+    has_many :facts, through: :users_facts
 end
