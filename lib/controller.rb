fc50ed1d801c0a11be663949f484f6606c01f7d4
github username <github email address>
2020-01-14 17:51:49 -0600

    Added functionality to the main menu and change user info.

--- lib/controller.rb
+++ lib/controller.rb
@@ -28,22 +28,18 @@ class IceBreaker
                 q.suffix 'Y/N'
             end
                 if yes_or_no
-                    puts "\n" * 35  
-                    puts "❄" * 119       
-                    fact = Fact.get_bday_fact(@@current_user.birthday)
-                    print "On #{Date::MONTHNAMES[@@current_user.birthday.split(',')[1].to_i].light_cyan}"
-                    print "\s#{@@current_user.birthday.split(',')[2].to_i},\s".light_cyan
-                    print "#{fact.year.light_cyan},\s\n"
-                    puts "#{fact.text}"
-                    puts "❄" * 119
-                    puts "\n" * 5
+                    puts "\n" * 35
+                    user_month = @@current_user.birthday.split(',')[1]
+                    user_day = @@current_user.birthday.split(',')[2]
+                    fact = [Fact.get_date_fact(user_month, user_day)]
+                    # binding.pry
+                    IceBreaker.display(fact)
                     save_fact = PROMPT.select("You can save this IceBreaker to your profile for quick reference by hitting the save button!
                     ", %W(Save Main))
                     case save_fact
                     when "Save"
                         # binding.pry                        
-                        FactUser.save_icebreaker(@@current_user.id,fact.id)
-                        # binding.pry
+                        FactUser.save_icebreaker(@@current_user.id,fact[0].id)
                         IceBreaker.main
                     else "Main"
                         IceBreaker.main
@@ -63,6 +59,11 @@ class IceBreaker
     def self.login
         find_user = PROMPT.ask("What is your username?".light_cyan, required: true)
         @@current_user = User.find_by(username: find_user)
+        if @@current_user == nil
+            puts "User not found".light_red
+            puts "Please try again!"
+            self.login
+        end
         enter_password = PROMPT.mask('password:', echo: true,required: true)
         # binding.pry
         if enter_password == @@current_user.password
@@ -79,43 +80,34 @@ class IceBreaker
     def self.main
         puts "\n" * 30
         IceBreaker.banner_icebreaker
-        menu_selection = PROMPT.select("Select from the following options?", %w(MyIceBreakers Random_Icebreaker EditMyInfo LogOut Exit))
+        menu_selection = PROMPT.select("Select from the following options?", %w(MyIceBreakers Random_Icebreaker DateIcebreaker YearIcebreaker EditMyInfo LogOut Exit))
         case menu_selection
         when "MyIceBreakers"
             menu_selection = PROMPT.select('Select from the following options.', %w(ShowAll DeleteAll Main))
             case menu_selection
             when "ShowAll"
-                puts "\n" * 30
-                binding.pry
+                puts "\n" * 35
                 IceBreaker.display(@@current_user.facts)
-                # my_icebreakers = User.my_icebreakers(@@current_user)
                 menu_selection = PROMPT.select('Select from the following options.', %w( Back))
                 case menu_selection
                 when "Back"
                     IceBreaker.main
                 end
             when "DeleteAll"
-                binding.pry
                 are_you_sure = PROMPT.yes?('Are you sure you want to delete all?') do |q|
                 q.suffix 'Y/N'
                     end
                 if are_you_sure
                 @@current_user.facts.delete_all
                 IceBreaker.main
-                binding.pry
                 else
                   IceBreaker.main
                 end
             else "Main"
                 IceBreaker.main
             end
-
-            #MyBirthday facts
-            # Returns all of users facts with number, number type, and text.
-            # Need ability to store facts Ive looked up
-            #--- Given the option to delete all IceBreakers
         when "Random_Icebreaker"
-             requested_type = PROMPT.select("Please select a category.", %w(Trivia Maths Date Year))
+             requested_type = PROMPT.select("Please select a category.", %w(Trivia Math))
             case requested_type
             when "Trivia"
                 trivia_fact = [Fact.get_random_fact(requested_type.downcase)]
@@ -125,30 +117,76 @@ class IceBreaker
                 save_or_exit = PROMPT.select("Save or exit?", %w(Save Exit))
                 case save_or_exit
                 when "Save"
-                # binding.pry
-                save_fact = FactUser.save_icebreaker(@@current_user.id,trivia_fact[0].id)
-                @@current_user.facts << trivia_fact
-                # binding.pry
-                IceBreaker.main
+                    # binding.pry
+                    save_fact = FactUser.save_icebreaker(@@current_user.id,trivia_fact[0].id)
+                    @@current_user.facts << trivia_fact
+                    # binding.pry
+                    IceBreaker.main
                 else "Exit"
-                IceBreaker.main
+                    IceBreaker.main
                 end
-            when "Maths"
-                Fact.get_math_fact
-            when "Date"
-                Fact.get_date_fact
-            else "Year"
-                Fact.get_year_fact
-            # Given option to select type TRIVIA, MATH, DATE, YEAR
+            else "Math"
+                trivia_fact = [Fact.get_random_fact(requested_type.downcase)]
+                # binding.pry
+                IceBreaker.display(trivia_fact)
+                save_or_exit = PROMPT.select("Save or exit?", %w(Save Exit))
+                case save_or_exit
+                when "Save"
+                    save_fact = FactUser.save_icebreaker(@@current_user.id,trivia_fact[0].id)
+                    @@current_user.facts << trivia_fact
+                    IceBreaker.main
+                else "Exit"
+                    IceBreaker.main
+                end            
             end
+        when "DateIcebreaker"
+            month = PROMPT.ask("Please enter a month (MM) of interest.".light_cyan, required: true) do |q|
+                q.in('01-12')
+            end
+            day = PROMPT.ask('Please enter the day (DD) of interest'.light_cyan, required: true) do |q|
+                q.in('01-31')
+            end
+            trivia_fact = [Fact.get_date_fact(month,day)]
+            IceBreaker.display(trivia_fact)
+            save_or_exit = PROMPT.select("Save or exit?", %w(Save Exit))
+            case save_or_exit
+            when "Save"
+              save_fact = FactUser.save_icebreaker(@@current_user.id,trivia_fact[0].id)
+              @@current_user.facts << trivia_fact
+              IceBreaker.main
+            else "Exit"
+              IceBreaker.main
+            end        
+        when "YearIcebreaker"
+            year = PROMPT.ask('Please enter the year (YYYY) of interest'.light_cyan, required: true) do |q|
+                q.in(1..Date.today.year)
+            end
+            trivia_fact = [Fact.get_year_fact(year)]
+            IceBreaker.display(trivia_fact)
+            save_or_exit = PROMPT.select("Save or exit?", %w(Save Exit))
+            case save_or_exit
+            when "Save"
+              save_fact = FactUser.save_icebreaker(@@current_user.id,trivia_fact[0].id)
+              @@current_user.facts << trivia_fact
+              IceBreaker.main
+            else "Exit"
+              IceBreaker.main
+            end        
         when "EditMyInfo"
-            # Given the option to change my user information USERNAME, PASSWORD, AND BIRTHDAY
+            pick_an_edit = PROMPT.select("Change Username or Password?", %w(Username Password))
+            case pick_an_edit
+            when "Username"
+                self.change_username
+                IceBreaker.main
+            else "Password"
+                self.change_password
+                puts "Your password was successfully"
+                IceBreaker.main              
+            end
         when "LogOut"
             @@current_user = nil
-            IceBreaker.main
-            # makes @user_logged_in = false            
+            IceBreaker.main           
         else "Exit"
-            #Set logged in status to false
            self.quit
         end
     end
@@ -184,25 +222,87 @@ class IceBreaker
         # binding.pry
         facts.each do |fact|
             if fact.fact_type == "trivia" 
+                puts "\n"
                 puts "❄" * 119
                 print "The number".light_cyan
                 puts "\s#{fact.number}".light_cyan
                 puts "refers to #{fact.text}"
                 puts "❄" * 119
-                puts "\n" * 1
+                puts "\n" 
             elsif fact.fact_type == "date"
+                puts "\n"
                 puts "❄" * 119
-                print "On #{Date::MONTHNAMES[@@current_user.birthday.split(',')[1].to_i].light_cyan}"
-                print "\s#{@@current_user.birthday.split(',')[2].to_i},\s".light_cyan
+                # binding.pry
+                #edit the month name and day
+                print "On #{Date::MONTHNAMES[fact.month_day.split(',')[0].to_i]}".light_cyan
+                # binding.pry
+                print "\s#{fact.month_day.split(',')[1].to_i},\s".light_cyan
                 print "#{fact.year.light_cyan},\s\n"
                 puts "#{fact.text}"
                 puts "❄" * 119
-                puts "\n" * 1
+                puts "\n"
+            elsif fact.fact_type == "math"
+                puts "\n"
+                puts "❄" * 119
+                print "In Math, the number".light_cyan
+                puts "\s#{fact.number}".light_cyan
+                puts "is #{fact.text}"
+                puts "❄" * 119
+                puts "\n" 
+            elsif fact.fact_type == "year"
+                puts "\n"
+                puts "❄" * 119
+                print "In the year #{fact.number}".light_cyan
+                puts ",\s#{fact.text}"
+                puts "❄" * 119
+                puts "\n" 
             end
             # binding.pry
         end
     end
-
+    def self.change_username
+        given_username = PROMPT.ask("What do you want your new User Name to be?", required: true)
+        confirm_username = PROMPT.yes?("#{given_username.light_green.bold} is what you entered. Are you sure?") do |q|
+                q.suffix 'Y/N'
+            end
+        if confirm_username
+            binding.pry
+            if User.find_by(username: given_username) == nil 
+                @@current_user.username = nil   
+                @@current_user.username = given_username
+                @@current_user.save
+            else
+                puts "#{given_username.light_red.bold} is already taken. Please choose a different username."
+                self.change_username
+            end
+        end
+    end
+    def self.change_password
+        old_password = PROMPT.mask("Please enter your old password".light_yellow, required: true)
+        if old_password == @@current_user.password
+            new_password = PROMPT.mask("Please enter your new password".light_cyan, required: true) do |q|
+            q.validate(/^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/)
+            q.messages[:valid?] = 'Your passowrd must be at least 6 characters and include one number and one letter'
+          end        
+            confirm_password = PROMPT.mask("Please confirm your new password".light_green, required: true)
+            # binding.pry
+            if new_password == confirm_password
+                puts "\n" * 35
+                @@current_user.password = nil
+                binding.pry
+                @@current_user.password = new_password
+                binding.pry
+            else
+                puts "\n" * 35
+                puts "Those didn't match. Please try again!".light_red
+                self.change_password
+            end
+        else
+            puts "That was not right.".light_red
+            puts "Please try again"
+            self.change_password
+        end
+    end
 end
 
 
--- lib/fact.rb
+++ lib/fact.rb
@@ -2,28 +2,39 @@ class Fact < ActiveRecord::Base
     has_many :fact_users
     has_many :users, through: :fact_users
 
-    def self.get_bday_fact(user_birthday)
-        # binding.pry
-        month = user_birthday.split(',')[1]
-        day= user_birthday.split(',')[2]
+    def self.get_date_fact(month,day)
         response = Unirest.get "https://numbersapi.p.rapidapi.com/#{month}/#{day}/date?fragment=true&json=true",
         headers:{
         "X-RapidAPI-Host" => "numbersapi.p.rapidapi.com",
         "X-RapidAPI-Key" => "03ad68a3d9msh715db69b24278b8p1c06acjsncb379bcf54f4"
         }
-        fact = Fact.find_or_create_by(text: response.body["text"]) do |fact|
+        date_fact = Fact.find_or_create_by(text: response.body["text"]) do |fact|
             fact.text = response.body["text"]
             fact.fact_type = response.body["type"]
             fact.number = response.body["number"]
-            fact.month_day = user_birthday.last(3)
+            fact.month_day = "#{month},#{day}"
             fact.year = response.body["year"]
             end
-        fact
+        date_fact
+    end
+
+    def self.get_year_fact(year)
+        response = Unirest.get "https://numbersapi.p.rapidapi.com/#{year}/year?fragment=true&json=true",
+        headers:{
+        "X-RapidAPI-Host" => "numbersapi.p.rapidapi.com",
+        "X-RapidAPI-Key" => "03ad68a3d9msh715db69b24278b8p1c06acjsncb379bcf54f4"
+        }
+        year_fact = Fact.find_or_create_by(text: response.body["text"]) do |fact|
+            fact.text = response.body["text"]
+            fact.fact_type = response.body["type"]
+            fact.number = response.body["number"]
+            fact.year = response.body["year"]
+        end
+        year_fact
     end
 
 
     def self.get_random_fact(fact_type)
-        # binding.pry
         response = Unirest.get "https://numbersapi.p.rapidapi.com/random/#{fact_type}?max=20&fragment=true&min=10&json=true",
         headers:{
           "X-RapidAPI-Host" => "numbersapi.p.rapidapi.com",
@@ -35,13 +46,6 @@ class Fact < ActiveRecord::Base
             fact.number = response.body["number"]
             fact.year = response.body["year"]
             end
-            # binding.pry
         random_fact 
-    end
-    def self.get_math_fact
-    end
-    def self.get_date_fact
-    end
-    def self.get_year_fact
     end
 end
